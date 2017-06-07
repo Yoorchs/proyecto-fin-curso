50b1552504c4b87c6a919f1ca917944f194f5930
Yoorchs <elmonker@gmail.com>
2017-06-07 17:53:17 +0100

    Primer despliegue

---
+++ src/main/java/com/ipartek/formacion/dbms/persistence/Curso.java
@@ -0,0 +1,14 @@
+package com.ipartek.formacion.dbms.persistence;
+
+import java.io.Serializable;
+
+public class Curso implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+}
